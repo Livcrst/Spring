ed57cae3ebbac88576ecbe6e974354b14b8ea357
lívia <livcristinna@gmail.com>
2021-09-17 22:11:43 -0300

    start controllers

--- api/src/main/java/br/com/livia/api/controllers/controller.java
+++ api/src/main/java/br/com/livia/api/controllers/controller.java
@@ -3,8 +3,10 @@ package br.com.livia.api.controllers;
 import java.util.Date;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestParam;
 // import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +18,11 @@ public class controller {
         return "Hello World";
     }
 
-    @PostMapping(value = "/")
-    public float dateString(@RequestBody float value) {
+    @PostMapping(value = "/count/{value}")
+    public float count_debt(@PathVariable("value") String value) {
 
-        float result = value + (value * (12 / 100));
+        float fValue = Float.valueOf(value).floatValue();
+        float result = fValue + (fValue * (12 / 100));
 
         return result;
 
